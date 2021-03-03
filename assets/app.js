1eafce960b68718fed7fdf0e5eb80e2d02a4e1ad
Melvin Kosisochukwu <melvinnnamani@gmail.com>
2021-03-03 18:56:09 +0100

    Alert uploaded

---
+++ assets/app.js
@@ -0,0 +1,61 @@
+window.onload = (event) => {
+  setTimeout(() => {
+    document.getElementById("preloader").classList.add("off");
+  }, 1000);
+};
+
+function contactForm(e) {
+  e.preventDefault();
+  const form = e.target.elements;
+  let formBtn;
+  Object.keys(form).forEach((key) => {
+    if (form[key].type === "submit") formBtn = form[key];
+  });
+  const { name, email, company, message } = e.target.elements;
+  const payload = {
+    Name: name.value,
+    Email: email.value,
+    Company: company.value,
+    Mesage: message.value,
+  };
+  submitContactForm(payload, formBtn);
+  e.target.reset();
+}
+
+async function submitContactForm(data, elem) {
+  const loader = document.createElement("img");
+  loader.width = 50;
+  loader.src = "assets/icon/spinner.svg";
+  elem.innerHTML = "";
+  elem.append(loader);
+  try {
+    await fetch("https://formspree.io/f/meqpbnvn", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    elem.innerHTML = elem.title;
+    customAlert("success", "Message successfully sent!");
+  } catch (e) {
+    console.log(e);
+    elem.innerHTML = elem.title;
+    customAlert("error", "Error sending message!");
+  }
+}
+
+function customAlert(type = "", message) {
+  const alertHolder = document.createElement("div");
+  alertHolder.innerHTML = `
+  <div class="alert ${type.toLowerCase()}">
+  <p class="message white fw-5">
+      ${message}
+  </p>
+</div>
+  `;
+  document.body.append(alertHolder);
+  setTimeout(() => {
+    alertHolder.remove();
+  }, 3000);
+}
